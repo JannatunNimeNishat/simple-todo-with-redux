96e6a11b2ebb226aadcf6db4db390480611d1bd4
JannatunNimeNishat <nishatnime100@gmail.com>
2023-08-17 12:51:31 +0600

    clear the initial state

--- src/Redux/filter/initialState.jsx
+++ src/Redux/filter/initialState.jsx
@@ -1,4 +1,4 @@
 export const initalState = {
     status:'All',
-    colors:['green']
+    colors:[]
 }
--- src/Redux/todo/initialState.jsx
+++ src/Redux/todo/initialState.jsx
@@ -1,4 +1,4 @@
 export const initalState = [
-    { id: 1, text: 'Learn React from Learn with Sumit Youtube Channel', completed: false, color: 'green' },
-    { id: 2, text: 'Learn redux', completed: false, color: 'yellow' }
+    /* { id: 1, text: 'Learn React from Learn with Sumit Youtube Channel', completed: false, color: 'green' },
+    { id: 2, text: 'Learn redux', completed: false, color: 'yellow' } */
 ]
